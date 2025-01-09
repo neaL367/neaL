3975beb17645018d78b150cdb7ebf5691d294eeb
me22mer <atichatfantasy@gmail.com>
2025-01-09 15:24:22 +0700

    add speed-insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { ViewTransitions } from "next-view-transitions";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 import Navbar from "@/components/navbar";
 
@@ -24,6 +25,7 @@ export default function RootLayout({
     <ViewTransitions>
       <html lang="en" className={inter.className}>
         <body className="flex flex-col min-h-screen bg-background text-foreground">
+          <SpeedInsights />
           <main className="w-full max-w-2xl mx-auto px-4 sm:px-6 lg:px-8 py-8 ">
             <Navbar />
             {children}
