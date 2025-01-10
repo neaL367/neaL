c62faa0bbd854d07a47b157cf5b17f694fd93a67
me22mer <atichatfantasy@gmail.com>
2025-01-11 01:12:18 +0700

    refactor(snow): enhance particles initialization and configuration logic

--- src/components/snow.tsx
+++ src/components/snow.tsx
@@ -1,70 +1,117 @@
 "use client";
 
-import { useCallback } from "react";
-import type { Engine } from "tsparticles-engine";
-import Particles from "react-tsparticles";
-import { loadSlim } from "tsparticles-slim";
+import { useEffect, useMemo, useState } from "react";
+import Particles, { initParticlesEngine } from "@tsparticles/react";
+import {
+  type Container,
+  type ISourceOptions,
+  MoveDirection,
+  OutMode,
+} from "@tsparticles/engine";
+// import { loadAll } from "@tsparticles/all"; // if you are going to use `loadAll`, install the "@tsparticles/all" package too.
+// import { loadFull } from "tsparticles"; // if you are going to use `loadFull`, install the "tsparticles" package too.
+import { loadSlim } from "@tsparticles/slim"; // if you are going to use `loadSlim`, install the "@tsparticles/slim" package too.
+// import { loadBasic } from "@tsparticles/basic"; // if you are going to use `loadBasic`, install the "@tsparticles/basic" package too.
 
 export default function Snow() {
-  const particlesInit = useCallback(async (engine: Engine) => {
-    await loadSlim(engine);
+  const [init, setInit] = useState(false);
+
+  // this should be run only once per application lifetime
+  useEffect(() => {
+    initParticlesEngine(async (engine) => {
+      // you can initiate the tsParticles instance (engine) here, adding custom shapes or presets
+      // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
+      // starting from v2 you can add only the features you need reducing the bundle size
+      //await loadAll(engine);
+      //await loadFull(engine);
+      await loadSlim(engine);
+      //await loadBasic(engine);
+    }).then(() => {
+      setInit(true);
+    });
   }, []);
 
-  // const particlesLoaded = useCallback(async (container: Container | undefined) => {
-  //   await console.log(container);
-  // }, []);
+  const particlesLoaded = async (container?: Container): Promise<void> => {
+    console.log(container);
+  };
 
-  return (
-    <div className="absolute top-0 left-0 w-full h-full">
+  const options: ISourceOptions = useMemo(
+    () => ({
+      fullScreen: { enable: false },
+      fpsLimit: 120,
+      detectRetina: true,
+      background: {
+        color: {
+          value: "transparent",
+        },
+      },
+      // interactivity: {
+      //   events: {
+      //     onHover: {
+      //       enable: true,
+      //       mode: "repulse",
+      //     },
+      //   },
+      //   modes: {
+      //     push: {
+      //       quantity: 4,
+      //     },
+      //     repulse: {
+      //       distance: 66,
+      //       duration: 0.66,
+      //     },
+      //   },
+      // },
+      particles: {
+        color: { value: "#000000" },
+        move: {
+          direction: MoveDirection.bottom,
+          enable: true,
+          outModes: {
+            default: OutMode.out,
+          },
+          speed: 0.66,
+        },
+        number: {
+          density: {
+            enable: true,
+            area: 800,
+          },
+          value: 266,
+        },
+        opacity: {
+          value: 0.8,
+        },
+        shape: {
+          type: "circle",
+        },
+        size: {
+          value: 3,
+        },
+        wobble: {
+          enable: true,
+          distance: 10,
+          speed: 1.66,
+        },
+        zIndex: {
+          value: { min: 0, max: 50 },
+        },
+      },
+    }),
+    []
+  );
+
+  if (init) {
+    return (
+      <div className="absolute top-0 left-0 w-full h-full">
       <Particles
         id="tsparticles"
-        init={particlesInit}
-        // loaded={particlesLoaded}
-        className="h-full"
-        options={{
-          fullScreen: { enable: false },
-          fpsLimit: 120,
-          detectRetina: true,
-          background: {
-            color: {
-              value: "transparent",
-            },
-          },
-          particles: {
-            color: { value: "#000000" },
-            move: {
-              direction: "bottom",
-              enable: true,
-              outModes: "out",
-              speed: 0.66,
-            },
-            number: {
-              density: {
-                enable: true,
-                area: 800,
-              },
-              value: 166,
-            },
-            opacity: {
-              value: 0.7,
-            },
-            shape: {
-              type: "circle",
-            },
-            size: {
-              value: 3,
-            },
-            wobble: {
-              enable: true,
-              distance: 10,
-              speed: 2,
-            },
-            zIndex: {
-              value: { min: 0, max: 50 },
-            },
-          },
-        }}
+        particlesLoaded={particlesLoaded}
+        options={options}
       />
-    </div>
-  );
+      </div>
+    );
+  }
+
+  return <></>;
 }
