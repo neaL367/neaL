2a66a83d0796cb85614eae53abd12553b0fe60ed
me22mer <atichatfantasy@gmail.com>
2025-02-28 15:10:46 +0700

    lazy loading to snow

---
+++ src/app/page.tsx
@@ -0,0 +1,29 @@
+"use client"
+import Image from "next/image";
+import dynamic from 'next/dynamic'
+
+import ConnectContent from "@/contents/connect.mdx";
+import IntroContent from "@/contents/introduce.mdx";
+
+const Snow = dynamic(() => import("@/components/snow"), { ssr: false })
+
+export default function Page() {
+  return (
+    <>
+      <div className="relative w-full h-full my-8 pt-6 overflow-hidden rounded-lg border-0 shadow-sm">
+        <Image
+          src="https://avatars.githubusercontent.com/u/73696671?v=4"
+          width={220}
+          height={220}
+          alt="atichat"
+          loading="lazy"
+          quality={90}
+          className="object-contain"
+        />
+        <Snow />
+      </div>
+      <IntroContent />
+      <ConnectContent />
+    </>
+  );
+}
