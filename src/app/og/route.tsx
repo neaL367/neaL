1a8edcbb513e3939ddb4e078d87e6869b87f4606
me22mer <atichatfantasy@gmail.com>
2025-03-07 01:55:50 +0700

    edit og route

--- src/app/og/route.tsx
+++ src/app/og/route.tsx
@@ -1,40 +1,19 @@
-/* eslint-disable @next/next/no-img-element */
-/* eslint-disable jsx-a11y/alt-text */
-// @ts-check
-
 import { ImageResponse } from "next/og";
-// App router includes @vercel/og.
-// No need to install it.
-
-export const runtime = "edge";
 
 export async function GET() {
-  const imageData = await fetch(
-    new URL("../../../public/og.png", import.meta.url)
-  ).then((res) => res.arrayBuffer());
-
-  const base64ImageData = Buffer.from(imageData).toString("base64");
-
   return new ImageResponse(
     (
       <div
         style={{
           display: "flex",
+          justifyContent: "center",
           background: "#f6f6f6",
           width: "100%",
           height: "100%",
           padding: "0px",
+          backgroundImage: "url(https://neal367.vercel.app/og.png)",
         }}
-      >
-        <img
-          width="1200"
-          height="630"
-          src={`data:image/png;base64,${base64ImageData}`}
-          style={{
-            objectFit: "cover",
-          }}
-        />
-      </div>
+      ></div>
     ),
     {
       width: 1200,
