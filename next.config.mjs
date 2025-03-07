ee68f1b7ea2ce6f224c159819474d31fa48e20df
me22mer <atichatfantasy@gmail.com>
2025-03-07 15:51:25 +0700

    try to fix image optimize in prod

--- next.config.mjs
+++ next.config.mjs
@@ -30,7 +30,8 @@ const nextConfig = {
     minimumCacheTTL: 2678400, 
     qualities: [25, 50, 75],
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
-    unoptimized: true,
+    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
+    unoptimized: false,
 
     formats: ["image/webp", "image/avif"],
   },
