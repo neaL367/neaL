d9daafb352aa3abb0ad9f109193222fb6e5b2ea8
me22mer <atichatfantasy@gmail.com>
2025-03-07 20:03:20 +0700

    update photo card

--- src/components/photo-card.tsx
+++ src/components/photo-card.tsx
@@ -15,8 +15,8 @@ function formatDate(date: Date): string {
 
 export function PhotoCard({ post, onClick }: PhotoCardProps) {
   return (
-    <div className="overflow-hidden">
-      <Card className="overflow-hidden border-0 h-full flex flex-col hover:shadow-md transition-shadow duration-200">
+    <div className="">
+      <Card className="overflow-hidden mt-6 pt-0 py-0 border-0 h-max flex flex-col hover:shadow-md transition-shadow duration-200">
         <div
           className="relative aspect-square cursor-pointer border"
           onClick={onClick}
@@ -33,9 +33,7 @@ export function PhotoCard({ post, onClick }: PhotoCardProps) {
         </div>
       </Card>
       <div className="pt-2">
-        <p className="text-xs text-muted-foreground">
-          {formatDate(post.timestamp)}
-        </p>
+        <p className="text-xs text-zinc-500">{formatDate(post.timestamp)}</p>
       </div>
     </div>
   );
